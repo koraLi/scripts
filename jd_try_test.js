b26aff2a847a0f9394ae3ce640142f00cfafbe75
koraLi <1908915162@qq.com>
2021-07-27 19:14:19 +0800

    111
    111

--- jd_try_test.js
+++ jd_try_test.js
@@ -532,8 +532,8 @@ function taskurl(url, goodId) {
 			'Cookie': $.cookie,
 			'Connection': 'keep-alive',
 			'Accept': '*/*',
-			'UserAgent': $.isNode() ? (process.env.JD_USER_AGENT ? process.env.JD_USER_AGENT : (require('./USER_AGENTS').USER_AGENT)) : ($.getdata('JDUA') ? $.getdata('JDUA') : "jdapp;iPhone;9.4.4;14.3;network/4g;Mozilla/5.0 (iPhone; CPU iPhone OS 14_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148;supportJDSHWK/1"),
-			'Accept-Language': 'zh-cn',
+			'user-agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1',
+         		'Accept-Language': 'zh-cn',
 			'Referer': goodId ? `https://try.m.jd.com/activity/?id=${goodId}` : undefined
 		},
 	}
