944b66f6e75032789e2c8d09e571929527eb6698
koraLi <1908915162@qq.com>
2021-09-09 22:02:33 +0800

    111
    111

--- jd_try_temp.js
+++ jd_try_temp.js
@@ -221,12 +221,7 @@ function requireConfig(){
             //IOS等用户直接用NobyDa的jd $.cookie
             $.cookiesArr = [$.getdata('CookieJD'), $.getdata('CookieJD2'), ...jsonParse($.getdata('CookiesJD') || "[]").map(item => item.cookie)].filter(item => !!item);
         }
-        if(typeof process.env.JD_TRY_WHITELIST === "undefined") args_xh.whiteList = false;
-        else args_xh.whiteList = process.env.JD_TRY_WHITELIST === 'true';
-        if(typeof process.env.JD_TRY_PLOG === "undefined") args_xh.printLog = true;
-        else args_xh.printLog = process.env.JD_TRY_PLOG === 'true';
-        if(typeof process.env.JD_TRY_PASSZC === "undefined") args_xh.passZhongCao = true;
-        else args_xh.passZhongCao = process.env.JD_TRY_PASSZC === 'true';
+
         console.log(`共${$.cookiesArr.length}个京东账号\n`)
         console.log('=====环境变量配置如下=====')
         console.log(`jdPrice: ${typeof args_xh.jdPrice}, ${args_xh.jdPrice}`)
